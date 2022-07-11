a732e6a2f37044adff6a0e043798942ddff10831
iricshkin <fokira@mail.ru>
2022-07-12 00:21:36 +0300

    Create models User and Subscribe

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import User, Subscribe
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    """Админка пользователя."""
+    list_display = ('id', 'username', 'email')
+    search_fields = ('username', 'email')
+    empty_value_display = '-пусто-'
+    list_filter = ('username', 'email')
+
+
+class SubscribeAdmin(admin.ModelAdmin):
+    """Админка подписок."""
+    list_display = ('user', 'author')
+    list_filter = ('user',)
+    search_fields = ('user',)
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Subscribe, SubscribeAdmin)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,73 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Модель пользователя."""
+    username = models.CharField(
+        db_index=True,
+        max_length=150,
+        unique=True,
+        verbose_name='Логин',
+        help_text='Введите логин'
+    )
+    email = models.EmailField(
+        db_index=True,
+        unique=True,
+        max_length=254,
+        verbose_name='Электронная почта',
+        help_text='Введите электронную почту'
+    )
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя',
+        help_text='Введите имя'
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия',
+        help_text='Введите фамилию'
+    )
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('username',)
+
+    def __str__(self) -> str:
+        return self.username
+
+
+class Subscribe(models.Model):
+    """Модель подписок."""
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        # related_name='follower',
+        verbose_name='Подписчик'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        # related_name='following',
+        verbose_name='Избранный автор'
+    )
+
+    class Meta:
+        # ordering = ('-created',)
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'], name ='unique_relationships'
+            ),
+            # models.CheckConstraint(
+            #     name='prevent_self_follow',
+            #     check=~models.Q(user=models.F('author')),
+            # ),
+        ]
+
+    def __str__(self) -> str:
+        return f'{self.user} подписан(а) на {self.author}'
