c0c73ef526bc477abd669d2b570369f0b3fd7d6f
iricshkin <fokira@mail.ru>
2022-07-30 20:57:01 +0300

    db change-2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
             'DB_ENGINE', default='django.db.backends.postgresql'
         ),
         'NAME': os.getenv('DB_NAME', default='postgres'),
-        'USER': os.getenv('POSTGRES_USER', default='user'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='password'),
         'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default='5432'),
