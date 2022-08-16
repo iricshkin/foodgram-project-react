3fbd6c7a8f2a1c75c9b80d41b5cc509d0b7e6d8f
iricshkin <fokira@mail.ru>
2022-08-16 21:53:43 +0300

    Change load_data.py

--- backend/recipes/management/commands/load_data.py
+++ backend/recipes/management/commands/load_data.py
@@ -6,7 +6,7 @@ from typing import Any, Optional
 from django.core.management.base import BaseCommand
 from recipes.models import Ingredient
 
-JSON_PATH = '../data/ingredients.json'
+JSON_PATH = './data/ingredients.json'
 
 
 class Command(BaseCommand):
